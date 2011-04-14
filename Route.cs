5872531cd68d652d43b25ab3dfca7456963949d0
Borisov Anton <antonhbborisov@gmail.com>
2011-04-14 17:32:15 +0400

    random route color improvements

--- Route.cs
+++ Route.cs
@@ -32,7 +32,10 @@ namespace GBusManager
             {
                 rs = s;
                 Random rndm = new Random();
-                color = Color.FromArgb(rndm.Next(255), rndm.Next(255), rndm.Next(255));
+                //color = RandomColor();
+                color = RandomColorHSV(rndm.Next(), rndm.NextDouble(), rndm.NextDouble());
+                Console.WriteLine("RandomColor {0}", color.ToString());
+
 
                 string[] ns = s.Split();
                 nodes = new int[ns.Length];
@@ -47,6 +50,48 @@ namespace GBusManager
                 Console.WriteLine("Empty route");
             }
 		}
+
+        Color RandomColor()
+        {
+            Random random = new Random();
+            int red = random.Next(random.Next(255));
+            int green = random.Next(random.Next(255));
+            int blue = random.Next(random.Next(255));
+            Color color;
+            color = Color.FromArgb(red, green, blue);
+            //color = Color.F
+            return color;
+        }
+
+        Color RandomColorHSV(double hue, double saturation, double value)
+        {
+
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)
+                return Color.FromArgb(255, v, t, p);
+            else if (hi == 1)
+                return Color.FromArgb(255, q, v, p);
+            else if (hi == 2)
+                return Color.FromArgb(255, p, v, t);
+            else if (hi == 3)
+                return Color.FromArgb(255, p, q, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, t, p, v);
+            else
+                return Color.FromArgb(255, v, p, q);
+            
+            
+
+             
+        }
 		
 		public Route(int[] nn){
 			nodes = nn;
