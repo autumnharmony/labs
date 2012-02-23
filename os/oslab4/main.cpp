3af2223948c4d1edb56679a0c3d1c66a1c37bd0c
Borisov Anton <antonhbborisov@gmail.com>
2012-02-23 22:02:46 +0400

    oslab4

---
+++ os/oslab4/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "deque.h"
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    deque d;
+    deque_init(&d, 4);
+    deque_push_front(&d,10);
+    deque_print(&d);
+    deque_push_back(&d,1);
+    deque_print(&d);
+    deque_push_back(&d,2);
+    deque_print(&d);
+    deque_push_back(&d,3);
+    deque_print(&d);
+    deque_push_back(&d,4);
+    deque_print(&d);
+    deque_push_front(&d,5);
+    deque_print(&d);
+
+    deque_push_front(&d,6);
+    deque_print(&d);
+
+    deque_push_front(&d,7);
+    deque_print(&d);
+
+    deque_push_front(&d,8);
+    deque_print(&d);
+
+    deque_push_front(&d,9);
+    deque_print(&d);
+
+
+    deque_print(&d);
+
+
+
+    return 0;
+}
